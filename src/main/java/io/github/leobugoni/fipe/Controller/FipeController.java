95167b8e968320c3c9dd27e2de2c4b209c02f585
Leonardo Bugoni <leobugoni@gmail.com>
2019-06-14 17:26:40 -0300

    Fix request method

--- src/main/java/io/github/leobugoni/fipe/Controller/FipeController.java
+++ src/main/java/io/github/leobugoni/fipe/Controller/FipeController.java
@@ -5,8 +5,9 @@ import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -14,22 +15,22 @@ import java.io.InputStreamReader;
 @Controller
 public class FipeController {
 
-    @PostMapping(value = "/fipe/consultar/")
+    @RequestMapping(value = "/fipe/consultar/", method = RequestMethod.POST, produces = "application/json")
     public String consultar(@RequestBody String parametros) {
         try {
             HttpPost request = new HttpPost("http://veiculos.fipe.org.br/api/veiculos/"+parametros);
-            request.addHeader("Content-Type", "application/json; charset=utf-8");
             request.addHeader("Host", request.getURI().getHost());
             request.addHeader("Referer", "http://www.fipe.org.br/pt-br/home");
-
-            HttpResponse response = HttpClientBuilder.create().build().execute(request);
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
-            StringBuilder result = new StringBuilder();
+            final HttpResponse response = HttpClientBuilder.create().build().execute(request);
+            final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+            final StringBuilder stringBuilder = new StringBuilder();
             String line;
             while ((line = bufferedReader.readLine()) != null) {
-                result.append(line);
+                stringBuilder.append(line);
             }
-            return new ObjectMapper().readTree(result.toString()).toString();
+            final String result = new ObjectMapper().readTree(stringBuilder.toString()).toString();
+            System.err.println(result);
+            return result;
         } catch (final Exception e) {
             e.printStackTrace();
             return "";
